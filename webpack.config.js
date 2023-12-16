9854c29cf31f7b4bae8524c17619c9de78001892
jens <1035788780@qq.com>
2023-12-16 17:14:20 +0800

    initial project

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    entry: './src/js/index.ts',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js"
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: "ts-loader"
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin(
+            {
+                template: "./src/index.html"
+            }
+        )
+    ]
+}
+
+
